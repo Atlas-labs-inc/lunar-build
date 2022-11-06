2b7295881d7f919849d49939b6bdfb9e086b81a3
Samee Siddiqui <sameezsiddiqui@gmail.com>
2022-11-06 04:54:37 -0800

    home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,123 @@
-import {
-  Link as ChakraLink,
-  Text,
-  Code,
-  List,
-  ListIcon,
-  ListItem,
-} from '@chakra-ui/react'
-import { CheckCircleIcon, LinkIcon } from '@chakra-ui/icons'
+import { Spacer, Flex } from '@chakra-ui/react';
+import { Container } from '../components/Container';
+import { UserPanel } from '../components/UserPanel';
+import { ServerInfo } from '../components/ServerInfo';
+import { User } from '../components/User';
+import { ChannelContent } from '../components/ChannelContent';
+import { WalletSetupModal } from '../components/WalletSetupModal';
+import { CreateChannelModal } from '../components/CreateChannelModal';
+import { SettingsModal } from '../components/SettingsModal';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers'
+import { useStore } from '../store';
+import channelManagerAbi from '../abis/channel_manager.json'
+import permissionAbi from '../abis/permission.json'
+import profileAbi from '../abis/profile.json'
+import { ProfileModal } from '../components/ProfileModal';
+import { ForceDarkMode } from '../components/ForceDarkMode'
+import { MetaMaskProvider } from "metamask-react";
+import { useMetaMask } from "metamask-react";
 
-import { Hero } from '../components/Hero'
-import { Container } from '../components/Container'
-import { Main } from '../components/Main'
-import { DarkModeSwitch } from '../components/DarkModeSwitch'
-import { CTA } from '../components/CTA'
-import { Footer } from '../components/Footer'
+const Home = () => {
+  const provider = useStore((state) => state.provider);
+  const setProvider = useStore((state) => state.setProvider);
+  const setContracts = useStore((state) => state.setContracts);
+  const setServer = useStore((state) => state.setServer);
+  const setCurrentChannel = useStore((state) => state.setCurrentChannel);
+  const setLoadingMessages = useStore((state) => state.setLoadingMessages);
+  const currentChannel = useStore((state) => state.currentChannel);
+  const server = useStore((state) => state.server);
 
-const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+    useEffect(() => {
+      if (provider === null) {
+        const provider = new ethers.providers.JsonRpcProvider(process.env.NEXT_PUBLIC_Pl2);
+        setProvider(provider);
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+        const channelManager = new ethers.Contract(process.env.NEXT_PUBLIC_CHANNEL_MANAGER_CONTRACT, channelManagerAbi, provider)
+        const permissions = new ethers.Contract(process.env.NEXT_PUBLIC_PERMISSION_CONTRACT, permissionAbi, provider)
+        const profile = new ethers.Contract(process.env.NEXT_PUBLIC_PROFILE_CONTRACT, profileAbi, provider)
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
-)
+        setContracts({
+          channelManager,
+          permissions,
+          profile
+        })
 
-export default Index
+        const channels = channelManager.getChannelNames()
+        const members = profile.getAllUsers()
+        const name = channelManager.global_channel_name()
+        const icon = channelManager.icon_link()
+        const banner = channelManager.banner_link()
+
+        Promise.all([channels, members, name, icon, banner]).then((v) => {
+          const serverMembers = v[1].map((member) => {
+            return {
+              name: member.username,
+              role: member.is_moderator ? 'admin' : 'member',
+              pfp: member.pfp_link,
+              bio: member.bio,
+              mainWallet: member.main_wallet,
+              operatorWallet: member.operator_wallet
+            }
+          })
+
+          setServer({
+            name: v[2],
+            icon: v[3],
+            banner: v[4],
+            channels: v[0],
+            members: serverMembers
+          })
+
+          const channelName = v[0][0]
+          channelManager.getMessagesPaginated(channelName, 0, 100).then((messages) => {
+            const parsedMessages = messages.map((message) => {
+              if (message.message === '') {
+                return null
+              }
+              console.log(message)
+              const userDetails = serverMembers.find((member) => member.name === message.username)
+              return {
+                id: message.id.toString(),
+                content: message.message,
+                author: {
+                  name: message.username,
+                  pfp: userDetails.pfp,
+                  role: userDetails.role,
+                },
+                timestamp: message.timestamp.toString(),
+                replyTo: message.reply_id?.toString(),
+              }
+            }).filter((message) => message !== null)
+
+            console.log(parsedMessages)
+            setCurrentChannel({
+              name: channelName,
+              messages: parsedMessages
+            })
+
+            setLoadingMessages(false)
+          })
+        })
+      }
+    }, [])
+
+    return (
+      <MetaMaskProvider>
+        <ForceDarkMode>
+          <Container>
+            <WalletSetupModal/>
+            <CreateChannelModal/>
+            <SettingsModal/>
+            <ProfileModal/>
+            <ServerInfo />
+            <ChannelContent />
+            <UserPanel />
+          </Container>
+        </ForceDarkMode>
+      </MetaMaskProvider>
+
+    );
+};
+
+export default Home;
